7aa028a0c538f8ed02d479216b6ff7b14d0aff91
marconius <notification@marco.link>
2020-01-23 16:07:20 -0500

    remove unnecesary files and make debug false

--- suggestionsapi/api/settings.py
+++ suggestionsapi/api/settings.py
@@ -14,7 +14,7 @@ PROJECT_DIR = os.path.dirname(BASE_DIR)
 SECRET_KEY = 'ipgqvql)!$e@u1ye26c614ys4t*g$f47rlb@1!t#y!w&pgl^um'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
 
 ALLOWED_HOSTS = ['*']
 
--- suggestionsapi/api/views.py
+++ suggestionsapi/api/views.py
@@ -7,7 +7,6 @@ from .apps import search_manager
 def suggest(request):
     if request.method in ['GET', 'POST']:
         results = search_manager.search(request.GET['q'])
-        # return HttpResponse(serialize(results), status=200)
         return HttpResponse(results, content_type='application/json', status=200)
 
     return HttpResponseNotAllowed(['GET', 'POST'])
